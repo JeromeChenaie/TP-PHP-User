e1caab36467e26d37be7ffaea0d5f49a87853be0
Jerome Chenaie <jeromechenaie@proton.me>
2024-11-20 10:59:16 +0100

    Create User controller with empty methods

---
+++ www/Controllers/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class User
+{
+    public function register(): void
+    {
+
+    }
+
+    public function login(): void
+    {
+
+    }
+
+    public function logout():void
+    {
+
+    }
+}
