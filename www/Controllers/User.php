dcf0162495bfffbe96289139bf67818fd4873d76
JeromeChenaie <jeromechenaie@proton.me>
2024-11-27 10:56:30 +0100

    Update register function in User Controller

--- www/Controllers/User.php
+++ www/Controllers/User.php
@@ -2,11 +2,39 @@
 
 namespace App\Controllers;
 
+use App\Core\View;
+
 class User
 {
     public function register(): void
     {
 
+        if (!empty($_POST)) {
+
+            $userValidator = new \App\Core\UserValidator();
+
+            $userValidator->cleanAndCheckEmail($_POST['email']);
+            $userValidator->cleanAndCheckFirstname($_POST['firstname']);
+            $userValidator->cleanAndCheckLastname($_POST['lastname']);
+            $userValidator->cleanAndCheckCountry($_POST['country']);
+            $userValidator->checkPassword($_POST['password'], $_POST['passwordConfirm']);
+
+            if (!empty($userValidator->errors)) {
+                $view = new View("User/register.php");
+                $view->addData("title", "Inscription");
+                $view->addData("description", "Page d'inscription du site");
+
+                $view->addData("errors", $userValidator->errors);
+                $view->addData("lastData", $userValidator->lastData);
+            } else {
+                // TODO: Ajout en BDD
+            }
+
+        } else {
+            $view = new View("User/register.php");
+            $view->addData("title", "Inscription");
+            $view->addData("description", "Page d'inscription du site");
+        }
     }
 
     public function login(): void
