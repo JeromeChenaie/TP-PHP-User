2c45913a2bb01fc5ab888b1cd25edb9acebac6c3
JeromeChenaie <jeromechenaie@proton.me>
2024-12-05 15:01:25 +0100

    Clean home function

--- www/Controllers/Main.php
+++ www/Controllers/Main.php
@@ -6,21 +6,14 @@ use App\Core\View;
 
 class Main
 {
-
-    public function home():void
+    public function home(): void
     {
         session_start();
-        var_dump($_SESSION);
         if (!isset($_SESSION['user'])){
-            header('Location: /login');
+            header('Location: /');
             exit;
         }
 
-        $username = $_SESSION['user']['username'];
         $view = new View("Main/home.php");
-        $view->addData("Nom :", $username);
-
-
     }
-
 }
