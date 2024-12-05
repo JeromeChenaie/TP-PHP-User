30b8416cc7cd9e4acfd441423a0f6216806ca308
JeromeChenaie <jeromechenaie@proton.me>
2024-12-05 15:13:53 +0100

    Clean login form

--- www/Views/User/login.php
+++ www/Views/User/login.php
@@ -1,24 +1,11 @@
-<div class="container d-flex justify-content-center align-items-center ">
-    <div class="card p-4 shadow" style="width: 100%; max-width: 400px; border-color: #006A67;">
-        
-        <h2 class="text-center mb-4" style="color: #006A67;">Connexion</h2>
+<form method="POST" action="">
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" required> <br>
+    <br>
 
-        <?php if(isset($error)): ?>
-            <div class="alert alert-danger text-center"><?php echo $error; ?></div>
-        <?php endif; ?>
+    <label for="password">Mot de passe</label>
+    <input type="password" id="password" name="password" required><br>
+    <br>
 
-        <form method="POST" action="login.php">
-            <div class="form-group mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" id="email" name="email" class="form-control" required>
-            </div>
-
-            <div class="form-group mb-3">
-                <label for="password" class="form-label">Mot de passe</label>
-                <input type="password" id="password" name="password" class="form-control" required>
-            </div>
-
-            <button type="submit" class="btn w-100" style="background-color: #006A67; color: white;">Se connecter</button>
-        </form>
-    </div>
-</div>
+    <button type="submit" >Se connecter</button>
+</form>
